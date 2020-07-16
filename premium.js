b6cd74782eafc1086062506b8307bf6442f09ac7
PutTheBaconOnSocialMedia <68369900+PutTheBaconOnSocialMedia@users.noreply.github.com>
2020-07-16 18:01:38 +1000

    Update premium.js

--- premium.js
+++ premium.js
@@ -153,8 +153,8 @@ SoundFiltersArray	= {
 '!popoff': 'http://okaycool.africa/u/bruh/cLHCtDw8XO.mp3',
 '!read': 'http://okaycool.africa/u/bruh/IDSL3XpKmo.mp3',
 '!yoo' : 'https://www.myinstants.com/media/sounds/yoooooooooooooooooooooooooooohhhhhhhhhhhhhhhhhhhhhhhhhhhhhh.mp3',
-'!didntask' : 'https://files.catbox.moe/kwvsot.mp3'
-,};
+'!didntask' : 'https://files.catbox.moe/kwvsot.mp3',
+};
 
 FFactsArray		= [
     'is gay', 'is a poliohaver', 'is a chad', 'sucks dick', 'is cute :3',
